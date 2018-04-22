dddddb6dc965b49fa847f49fba8d486225be7449
Alex Herdzik <alex.j.herdzik@gmail.com>
2018-04-22 16:01:12 -0700

    Update cache automatically every 5 minutes

--- pricecache.js
+++ pricecache.js
@@ -1,10 +1,25 @@
 const get = require('simple-get');
 const asset = require('./asset.js')
 var assetPriceMap = new Map();
+var updateTimeout = 300 * 1000; //update every 5 minutes
 class PriceCache{
     constructor() 
     {
+        setInterval(this.updateCache, updateTimeout, "");
     }
+};
+
+PriceCache.prototype.updateCache = function()
+{
+    Object.keys(assetPriceMap).forEach(function(k)
+    {
+       get.concat(getStockRetrievalUrl(k), function (err, res, data) {
+          if (err) throw err
+          var val = parseFloat(data.toString());
+          assetPriceMap[k] = new asset.Asset(asset.AssetType.STOCK, k ,val, val);
+          console.log("NEW PRICE FOR "+ k + ": " + val);
+        })
+    });
 };
 
 PriceCache.prototype.initialize = function(stocksToGrab, bot) 
